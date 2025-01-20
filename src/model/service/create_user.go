eee182b949dffab5d1a4f9b9eca155590f1525b7
Rogerio Baccaglini <Rogerio.Baccaglini.Contractor@pepsico.com>
2025-01-20 16:05:55 -0300

    validate email uniqueness before creating user

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"net/http"
+
 	"github.com/rbaccaglini/simple_crud_golang/src/configuration/rest_err"
 	"github.com/rbaccaglini/simple_crud_golang/src/logger"
 	"github.com/rbaccaglini/simple_crud_golang/src/model"
@@ -11,6 +13,22 @@ func (ud *userDomainService) CreateUserService(
 	userDomain model.UserDomainInterface,
 ) (model.UserDomainInterface, *rest_err.RestErr) {
 	logger.Info("Creating user", zap.String("journey", "CreateUser"))
+
+	user, err := ud.FindUserByEmailService(userDomain.GetEmail())
+	if err != nil {
+		if err.Code == http.StatusNotFound {
+			logger.Error("Email is already registered", err, zap.String("journey", "CreateUser"))
+			return nil, rest_err.NewBadRequestError("Email is already registered")
+		}
+		logger.Error("Error on verify email", err, zap.String("journey", "CreateUser"))
+		return nil, rest_err.NewInternalServerError("Error on verify email")
+	}
+
+	if user != nil {
+		logger.Error("Email is already registered", err, zap.String("journey", "CreateUser"))
+		return nil, rest_err.NewBadRequestError("Email is already registered")
+	}
+
 	userDomain.EncryptPassword()
 
 	userDomainRepository, err := ud.userRepo.CreateUser(userDomain)
